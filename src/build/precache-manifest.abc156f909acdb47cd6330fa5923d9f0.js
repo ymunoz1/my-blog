6205bee8cccbdf3a15edf9f8c016af6c442694db
Yamil Muñoz Gomez <ingyamilmunoz@gmail.com>
2020-06-14 19:49:32 -0400

    First commit

---
+++ src/build/precache-manifest.abc156f909acdb47cd6330fa5923d9f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e081e6b3ecaa6a496db0cef9e94ab598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ad6861a988feb2fd889",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "82338777957a3a136d00",
+    "url": "/static/js/2.1dce0312.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1dce0312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad6861a988feb2fd889",
+    "url": "/static/js/main.8c9b1c5c.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
